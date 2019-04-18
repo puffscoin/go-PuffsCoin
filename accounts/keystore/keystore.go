a3bd03f68bd7ad4c440a902e4caac907a247c425
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-18 15:57:48 -0300

    go-ethereum to go-puffscoin

--- accounts/keystore/keystore.go
+++ accounts/keystore/keystore.go
@@ -33,11 +33,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/event"
+	"github.com/puffscoin/go-puffscoin/accounts"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/core/types"
+	"github.com/puffscoin/go-puffscoin/crypto"
+	"github.com/puffscoin/go-puffscoin/event"
 )
 
 var (
