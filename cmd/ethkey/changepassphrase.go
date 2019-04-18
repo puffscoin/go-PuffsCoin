d8b1d2bcc924b1cd81d6f5586c1a8093b4dc0b6b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-18 16:29:57 -0300

    go-ethereum to go-puffscoin

--- cmd/ethkey/changepassphrase.go
+++ cmd/ethkey/changepassphrase.go
@@ -5,8 +5,8 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/puffscoin/go-puffscoin/accounts/keystore"
+	"github.com/puffscoin/go-puffscoin/cmd/utils"
 	"gopkg.in/urfave/cli.v1"
 )
 
