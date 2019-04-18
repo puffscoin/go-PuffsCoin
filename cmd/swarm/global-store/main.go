f5aa2190ad548a8a4c91c24d7b9e61086a881b1b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-18 20:39:31 -0300

    ethereum to puffscoin

--- cmd/swarm/global-store/main.go
+++ cmd/swarm/global-store/main.go
@@ -1,25 +1,25 @@
-// Copyright 2019 The go-ethereum Authors
+// Copyright 2019 The go-puffscoin Authors
 // This file is part of go-ethereum.
 //
-// go-ethereum is free software: you can redistribute it and/or modify
+// go-puffscoin is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-ethereum is distributed in the hope that it will be useful,
+// go-puffscoin is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+// along with go-puffscoin. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
 import (
 	"os"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/log"
 	cli "gopkg.in/urfave/cli.v1"
 )
 
