e24f59df2fdcf4433d27ad044e084489e1d8ded1
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-18 15:54:57 -0300

    go-ethereum to go-puffscoin

--- accounts/keystore/account_cache_test.go
+++ accounts/keystore/account_cache_test.go
@@ -29,8 +29,8 @@ import (
 
 	"github.com/cespare/cp"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/accounts"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/puffscoin/go-puffscoin/accounts"
+	"github.com/puffscoin/go-puffscoin/common"
 )
 
 var (
