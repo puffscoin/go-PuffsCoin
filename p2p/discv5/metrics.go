98e45497345342148d234b0c2bcf2afef98b633c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:00:16 -0300

    eth to puffs

--- p2p/discv5/metrics.go
+++ p2p/discv5/metrics.go
@@ -1,6 +1,6 @@
 package discv5
 
-import "github.com/ethereum/go-ethereum/metrics"
+import "github.com/puffscoin/go-puffscoin/metrics"
 
 var (
 	ingressTrafficMeter = metrics.NewRegisteredMeter("discv5/InboundTraffic", nil)
