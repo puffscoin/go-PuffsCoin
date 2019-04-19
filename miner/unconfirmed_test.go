75c37ec73f3be3e5a7b635a2b0e7c3e58a8d919e
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 14:38:36 -0300

    eth to puffs

--- miner/unconfirmed_test.go
+++ miner/unconfirmed_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,8 +19,8 @@ package miner
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/core/types"
 )
 
 // noopChainRetriever is an implementation of headerRetriever that always
