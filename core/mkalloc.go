fb156109626c007b1a5915ef01f437351a1ef3be
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 01:04:19 -0300

    ethereum to puffscoin

--- core/mkalloc.go
+++ core/mkalloc.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -34,8 +34,8 @@ import (
 	"sort"
 	"strconv"
 
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/core"
+	"github.com/puffscoin/go-puffscoin/rlp"
 )
 
 type allocItem struct{ Addr, Balance *big.Int }
