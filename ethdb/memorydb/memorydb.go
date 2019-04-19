4e49bcc27eb457b2f26eb223bbe61102e5cf8064
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 12:20:59 -0300

    eth to puffs

--- ethdb/memorydb/memorydb.go
+++ ethdb/memorydb/memorydb.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,8 +23,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/ethdb"
 )
 
 var (
