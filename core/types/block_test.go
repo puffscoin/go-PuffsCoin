7f0186e911a799607ed64742ef301a8681777647
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:12:18 -0300

    ethereum to puffscoin

--- core/types/block_test.go
+++ core/types/block_test.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,8 +22,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/rlp"
 )
 
 // from bcValidBlockTest.json, "SimpleTx"
