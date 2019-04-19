599a0c601f6c0e0db8e1939bebfd9a8a5dd079b8
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:28:59 -0300

    eth to puffs

--- p2p/simulations/connect.go
+++ p2p/simulations/connect.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ import (
 	"errors"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/puffscoin/go-puffscoin/p2p/enode"
 )
 
 var (
