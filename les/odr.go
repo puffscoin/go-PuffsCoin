550984b73f6afbd97d29a22a9a291e455e5408ca
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 13:17:22 -0300

    eth to puffs

--- les/odr.go
+++ les/odr.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,10 +19,10 @@ package les
 import (
 	"context"
 
-	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/light"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/core"
+	"github.com/puffscoin/go-puffscoin/ethdb"
+	"github.com/puffscoin/go-puffscoin/light"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 // LesOdr implements light.OdrBackend
