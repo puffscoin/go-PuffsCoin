14d1a68d459f9f492d34ca2894e587d7005391e0
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:56:32 -0300

    ethereum to puffscoin

--- core/events.go
+++ core/events.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -17,8 +17,8 @@
 package core
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/core/types"
 )
 
 // NewTxsEvent is posted when a batch of transactions enter the transaction pool.
