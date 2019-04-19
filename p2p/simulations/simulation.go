f36e3a5e157de231732bed8aecdbeefb907cdebc
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:34:16 -0300

    eth to puffs

--- p2p/simulations/simulation.go
+++ p2p/simulations/simulation.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/puffscoin/go-puffscoin/p2p/enode"
 )
 
 // Simulation provides a framework for running actions in a simulated network
