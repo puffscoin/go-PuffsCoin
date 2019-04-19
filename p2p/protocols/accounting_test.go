5ff3cfe09da430ccbca38aeaed12105516e9764d
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:21:15 -0300

    eth to puffs

--- p2p/protocols/accounting_test.go
+++ p2p/protocols/accounting_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,9 +19,9 @@ package protocols
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/p2p/simulations/adapters"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/p2p"
+	"github.com/puffscoin/go-puffscoin/p2p/simulations/adapters"
+	"github.com/puffscoin/go-puffscoin/rlp"
 )
 
 //dummy Balance implementation
