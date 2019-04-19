dc42f17472fd1f1ac968d179113329354607e310
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:15:33 -0300

    ethereum to puffscoin

--- core/types/gen_log_json.go
+++ core/types/gen_log_json.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"errors"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
 )
 
 var _ = (*logMarshaling)(nil)
