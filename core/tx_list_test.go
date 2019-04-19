0bd62bf44012ba200c5195a6b38d15bff988d9db
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 01:08:54 -0300

    ethereum to puffscoin

--- core/tx_list_test.go
+++ core/tx_list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,8 +20,8 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/puffscoin/go-puffscoin/core/types"
+	"github.com/puffscoin/go-puffscoin/crypto"
 )
 
 // Tests that transactions can be added to strict lists and list contents and
