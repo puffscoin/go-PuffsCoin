d74bac13df35d43be90997787597512056e6ff23
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:16:00 -0300

    eth to puffs

--- p2p/enr/entries.go
+++ p2p/enr/entries.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +21,7 @@ import (
 	"io"
 	"net"
 
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/rlp"
 )
 
 // Entry is implemented by known node record entry types.
