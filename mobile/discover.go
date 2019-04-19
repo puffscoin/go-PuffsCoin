3a2c5e0b7df92177926e867d7075339cca98d091
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 14:52:23 -0300

    eth to puffs

--- mobile/discover.go
+++ mobile/discover.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,7 +22,7 @@ package geth
 import (
 	"errors"
 
-	"github.com/ethereum/go-ethereum/p2p/discv5"
+	"github.com/puffscoin/go-puffscoin/p2p/discv5"
 )
 
 // Enode represents a host on the network.
