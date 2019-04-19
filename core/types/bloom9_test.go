c1611da401c1ff897d8a17e84a7f0e6cccf414e4
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:13:31 -0300

    ethereum to puffscoin

--- core/types/bloom9_test.go
+++ core/types/bloom9_test.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -54,7 +54,7 @@ func TestBloom(t *testing.T) {
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/puffscoin/go-puffscoin/core/state"
 )
 
 func TestBloom9(t *testing.T) {
