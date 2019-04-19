834bfc8b17124fc11b90fb2672c9df993ee83e87
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:40:12 -0300

    ethereum to puffscoin

--- core/vm/logger_json.go
+++ core/vm/logger_json.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of go-ethereum.
+// This file is part of go-puffscoin.
 //
 // go-ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -22,8 +22,8 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/math"
 )
 
 type JSONLogger struct {
