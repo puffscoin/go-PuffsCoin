38b0fae4d14132282f823037a657ae660b2b4530
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 01:07:13 -0300

    ethereum to puffscoin

--- core/tx_cacher.go
+++ core/tx_cacher.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ package core
 import (
 	"runtime"
 
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/puffscoin/go-puffscoin/core/types"
 )
 
 // senderCacher is a concurrent transaction sender recoverer and cacher.
