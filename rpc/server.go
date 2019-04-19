b5e9cc301654138f27b44c684c7010c278216976
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 17:10:36 -0300

    eth to puffs

--- rpc/server.go
+++ rpc/server.go
@@ -1,5 +1,5 @@
 // Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,7 +22,7 @@ import (
 	"sync/atomic"
 
 	mapset "github.com/deckarep/golang-set"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 const MetadataApi = "rpc"
