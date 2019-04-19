360ea8bda503943449cc0b9d6d0fe141921dfce7
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 18:58:21 -0300

    eth to puffs

--- signer/storage/aes_gcm_storage.go
+++ signer/storage/aes_gcm_storage.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of go-ethereum.
+// This file is part of go-puffscoin.
 //
 // go-ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -26,7 +26,7 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 type storedCredential struct {
