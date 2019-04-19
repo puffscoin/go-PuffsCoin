1b5958a9a4b36505fcbafb05573d72806ab0a797
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-18 22:47:35 -0300

    ethereum to puffscoin

--- core/asm/compiler.go
+++ core/asm/compiler.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,8 +22,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/puffscoin/go-puffscoin/common/math"
+	"github.com/puffscoin/go-puffscoin/core/vm"
 )
 
 // Compiler contains information about the parsed source
