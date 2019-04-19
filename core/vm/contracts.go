3b00f92877efc0cd6c05cd210d73c9b35c936b91
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:30:08 -0300

    ethereum to puffscoin

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -21,11 +21,11 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/crypto/bn256"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/math"
+	"github.com/puffscoin/go-puffscoin/crypto"
+	"github.com/puffscoin/go-puffscoin/crypto/bn256"
+	"github.com/puffscoin/go-puffscoin/params"
 	"golang.org/x/crypto/ripemd160"
 )
 
