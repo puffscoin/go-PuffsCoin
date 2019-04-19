c5a1b7ec4dea14ddb428b04f56531e3fdafc8f1a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:26:54 -0300

    ethereum to puffscoin

--- core/vm/runtime/runtime_example_test.go
+++ core/vm/runtime/runtime_example_test.go
@@ -1,5 +1,5 @@
 // Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,8 +19,8 @@ package runtime_test
 import (
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/vm/runtime"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/core/vm/runtime"
 )
 
 func ExampleExecute() {
