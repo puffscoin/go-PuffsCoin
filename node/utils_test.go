91680fc2b09ec2c5ef06f14f9d0e0df4b6e183f7
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 15:25:33 -0300

    eth to puffs

--- node/utils_test.go
+++ node/utils_test.go
@@ -1,5 +1,5 @@
 // Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,8 +22,8 @@ package node
 import (
 	"reflect"
 
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/puffscoin/go-puffscoin/p2p"
+	"github.com/puffscoin/go-puffscoin/rpc"
 )
 
 // NoopService is a trivial implementation of the Service interface.
