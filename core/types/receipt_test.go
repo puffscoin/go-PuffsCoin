3b510a83b07e6999df68ead725f4228094f2f7dc
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:20:51 -0300

    ethereum to puffscoin

--- core/types/receipt_test.go
+++ core/types/receipt_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,10 +23,10 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/params"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/crypto"
+	"github.com/puffscoin/go-puffscoin/params"
+	"github.com/puffscoin/go-puffscoin/rlp"
 )
 
 func TestLegacyReceiptDecoding(t *testing.T) {
