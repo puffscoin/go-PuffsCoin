2446c73e2dd7fd8b1a21388bbf3f074b27bceb73
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 01:08:26 -0300

    ethereum to puffscoin

--- core/tx_list.go
+++ core/tx_list.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,9 +22,9 @@ import (
 	"math/big"
 	"sort"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/core/types"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 // nonceHeap is a heap.Interface implementation over 64bit unsigned integers for
