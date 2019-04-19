89c3eb39b36607f00bd69104a518b2e05105e386
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 14:20:46 -0300

    eth to puffs

--- metrics/influxdb/influxdb.go
+++ metrics/influxdb/influxdb.go
@@ -5,8 +5,8 @@ import (
 	uurl "net/url"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/puffscoin/go-puffscoin/log"
+	"github.com/puffscoin/go-puffscoin/metrics"
 	"github.com/influxdata/influxdb/client"
 )
 
