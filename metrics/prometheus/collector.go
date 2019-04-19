a97be5acfe9a42735d27207a66cd6026a177ecd3
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 14:22:24 -0300

    eth to puffs

--- metrics/prometheus/collector.go
+++ metrics/prometheus/collector.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,7 +22,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/puffscoin/go-puffscoin/metrics"
 )
 
 var (
