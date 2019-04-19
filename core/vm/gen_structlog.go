9ed04bdeda02ec7172b927595ff307b7961a0cf4
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:34:39 -0300

    ethereum to puffscoin

--- core/vm/gen_structlog.go
+++ core/vm/gen_structlog.go
@@ -6,9 +6,9 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common/math"
 )
 
 var _ = (*structLogMarshaling)(nil)
