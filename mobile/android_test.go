4a0599ff46f6f01c331f82e34b33438c8b3dc144
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 14:49:14 -0300

    eth to puffs
    still ned to set up artifacts via org.puffscoin/gpuffs

--- mobile/android_test.go
+++ mobile/android_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -25,7 +25,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/internal/build"
+	"github.com/puffscoin/go-puffscoin/internal/build"
 )
 
 // androidTestClass is a Java class to do some lightweight tests against the Android
