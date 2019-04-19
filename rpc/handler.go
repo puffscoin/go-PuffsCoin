42481b4eb7b045c236948d7f3d4a75f84291dcb7
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 17:08:05 -0300

    eth to puffs

--- rpc/handler.go
+++ rpc/handler.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -25,7 +25,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 // handler handles JSON-RPC messages. There is one handler per connection. Note that
