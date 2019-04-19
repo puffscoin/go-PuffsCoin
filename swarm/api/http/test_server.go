2e9b2d8e2768d6af631f0a4bcb49d81374aae542
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 19:09:07 -0300

    eth to puffs

--- swarm/api/http/test_server.go
+++ swarm/api/http/test_server.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,9 +23,9 @@ import (
 	"os"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/api"
-	"github.com/ethereum/go-ethereum/swarm/storage"
-	"github.com/ethereum/go-ethereum/swarm/storage/feed"
+	"github.com/puffscoin/go-puffscoin/swarm/api"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/feed"
 )
 
 type TestServer interface {
