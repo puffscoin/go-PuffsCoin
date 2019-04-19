c714bab737a9a16f836f22c26c84b8ebf78a9f9d
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:32:25 -0300

    eth to puffs

--- p2p/simulations/mocker_test.go
+++ p2p/simulations/mocker_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -27,7 +27,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/puffscoin/go-puffscoin/p2p/enode"
 )
 
 func TestMocker(t *testing.T) {
