d7da5cae0259df1901976c392f50444f732c12d1
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:33:45 -0300

    ethereum to puffscoin

--- core/vm/gas_table.go
+++ core/vm/gas_table.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -17,9 +17,9 @@
 package vm
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/math"
+	"github.com/puffscoin/go-puffscoin/params"
 )
 
 // memoryGasCost calculates the quadratic gas for memory expansion. It does so
