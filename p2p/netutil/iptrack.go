a9e5b93d9483ac3c9f9fe1ca4ca5b8300581884b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:17:35 -0300

    eth to puffs

--- p2p/netutil/iptrack.go
+++ p2p/netutil/iptrack.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ package netutil
 import (
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/mclock"
+	"github.com/puffscoin/go-puffscoin/common/mclock"
 )
 
 // IPTracker predicts the external endpoint, i.e. IP address and port, of the local host
