5563f9206de45abd7b94f3d8c9d7609fc94399b5
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 15:03:38 -0300

    eth to puffs

--- mobile/init.go
+++ mobile/init.go
@@ -16,13 +16,13 @@
 
 // Contains initialization code for the mbile library.
 
-package geth
+package gpuffs
 
 import (
 	"os"
 	"runtime"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 func init() {
