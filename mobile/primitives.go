e643ab3920d64a8a5c752b0c63e7cc1fcae0751b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 15:07:25 -0300

    geth to gpuffs

--- mobile/primitives.go
+++ mobile/primitives.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -16,7 +16,7 @@
 
 // Contains various wrappers for primitive types.
 
-package geth
+package gpuffs
 
 import (
 	"errors"
