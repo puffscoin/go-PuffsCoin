9e0ff8df83beef0f1f12e3c5ebd4872dd793eaba
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:23:44 -0300

    eth to puffs

--- p2p/protocols/reporter.go
+++ p2p/protocols/reporter.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,8 +20,8 @@ import (
 	"encoding/binary"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/puffscoin/go-puffscoin/log"
+	"github.com/puffscoin/go-puffscoin/metrics"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
