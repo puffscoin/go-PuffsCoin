4890be4b56171674df9f32d97c2cbd09bd9c314a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 17:00:58 -0300

    eth to puffs

--- params/dao.go
+++ params/dao.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ package params
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/puffscoin/go-puffscoin/common"
 )
 
 // DAOForkBlockExtra is the block header extra-data field to set for the DAO fork
