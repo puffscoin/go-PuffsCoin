20d748c7ec91854e5f3e205e4b3597036d625696
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:07:00 -0300

    eth to puffs

--- p2p/discv5/topic.go
+++ p2p/discv5/topic.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,8 +23,8 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/mclock"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/common/mclock"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 const (
