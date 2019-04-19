5169170fbb4f237e88ad6b55875dc04884b00c52
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:02:31 -0300

    eth to puffs

--- p2p/discv5/node.go
+++ p2p/discv5/node.go
@@ -1,5 +1,5 @@
 // Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -30,8 +30,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/crypto"
 )
 
 // Node represents a host on the network.
