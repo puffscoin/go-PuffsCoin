cf1da228102f65b76a3951deb4fd6c25d190d3da
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 12:52:11 -0300

    eth to puffs

--- les/flowcontrol/manager.go
+++ les/flowcontrol/manager.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,8 +22,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/mclock"
-	"github.com/ethereum/go-ethereum/common/prque"
+	"github.com/puffscoin/go-puffscoin/common/mclock"
+	"github.com/puffscoin/go-puffscoin/common/prque"
 )
 
 // cmNodeFields are ClientNode fields used by the client manager
