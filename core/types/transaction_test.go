ddbf2862dcf78f5ca6c2ab9744a53494128d68d9
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:24:36 -0300

    ethereum to puffscoin

--- core/types/transaction_test.go
+++ core/types/transaction_test.go
@@ -1,5 +1,5 @@
 // Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,9 +23,9 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/crypto"
+	"github.com/puffscoin/go-puffscoin/rlp"
 )
 
 // The values in those tests are from the Transaction Tests
