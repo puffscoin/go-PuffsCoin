76d101b564707260c328ce85a3cb59ba6c587681
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 17:06:47 -0300

    eth to puffs

--- rpc/client_test.go
+++ rpc/client_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -31,7 +31,7 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/puffscoin/go-puffscoin/log"
 )
 
 func TestClientRequest(t *testing.T) {
