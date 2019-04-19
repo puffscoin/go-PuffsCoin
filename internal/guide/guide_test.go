d7477e0f215db1364d2d9dd59623d36efcce74ef
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 12:48:05 -0300

    eth to puffs

--- internal/guide/guide_test.go
+++ internal/guide/guide_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -30,8 +30,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/puffscoin/go-puffscoin/accounts/keystore"
+	"github.com/puffscoin/go-puffscoin/core/types"
 )
 
 // Tests that the account management snippets work correctly.
