61af5d67ba9603e1c5665b0bdeb3b05b1566737a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:12:25 -0300

    eth to puffs

--- p2p/enode/node_test.go
+++ p2p/enode/node_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/p2p/enr"
-	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/puffscoin/go-puffscoin/p2p/enr"
+	"github.com/puffscoin/go-puffscoin/rlp"
 	"github.com/stretchr/testify/assert"
 )
 
