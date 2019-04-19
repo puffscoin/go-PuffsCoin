d4b88f62161968a2befb7a7589d81fdc76d47fda
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:42:11 -0300

    ethereum to puffscoin

--- core/vm/stack_table.go
+++ core/vm/stack_table.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
 package vm
 
 import (
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/puffscoin/go-puffscoin/params"
 )
 
 func minSwapStack(n int) int {
