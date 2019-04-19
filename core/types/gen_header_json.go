7149052adc4740afe308aa75020433588e44f379
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:15:00 -0300

    ethereum to puffscoin

--- core/types/gen_header_json.go
+++ core/types/gen_header_json.go
@@ -7,8 +7,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
 )
 
 var _ = (*headerMarshaling)(nil)
