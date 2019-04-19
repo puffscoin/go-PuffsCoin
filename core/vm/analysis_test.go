d9342777c0edd7fa76f22b771922de87042fa696
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:28:50 -0300

    ethereum to puffscoin

--- core/vm/analysis_test.go
+++ core/vm/analysis_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ package vm
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/puffscoin/go-puffscoin/crypto"
 )
 
 func TestJumpDestAnalysis(t *testing.T) {
