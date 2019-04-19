9d1c328212295e2e74fd19ad653a6f7da9498876
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 17:06:21 -0300

    eth to puffs

--- rpc/client_example_test.go
+++ rpc/client_example_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,7 +22,7 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/puffscoin/go-puffscoin/rpc"
 )
 
 // In this example, our client wishes to track the latest 'block number'
