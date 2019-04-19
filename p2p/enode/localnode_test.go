80fccdee4e3b65b36a9c0b5a08aa55189b24ba5a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 16:11:25 -0300

    eth to puffs

--- p2p/enode/localnode_test.go
+++ p2p/enode/localnode_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,8 +19,8 @@ package enode
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/p2p/enr"
+	"github.com/puffscoin/go-puffscoin/crypto"
+	"github.com/puffscoin/go-puffscoin/p2p/enr"
 )
 
 func newLocalNodeForTesting() (*LocalNode, *DB) {
