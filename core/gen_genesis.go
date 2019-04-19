87a9f94a5356b75e59784fd05b5e863ff62c6081
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-19 00:58:51 -0300

    ethereum to puffscoin

--- core/gen_genesis.go
+++ core/gen_genesis.go
@@ -7,10 +7,10 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common/math"
+	"github.com/puffscoin/go-puffscoin/params"
 )
 
 var _ = (*genesisSpecMarshaling)(nil)
