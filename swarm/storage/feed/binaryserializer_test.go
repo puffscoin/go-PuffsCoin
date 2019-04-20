7e433c2856d469dc91502ef32738cb1514340aa0
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:07:52 -0300

    eth to puffs

--- swarm/storage/feed/binaryserializer_test.go
+++ swarm/storage/feed/binaryserializer_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +21,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
 )
 
 // KV mocks a key value store
