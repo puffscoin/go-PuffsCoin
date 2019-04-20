c6f5a3a9e9b1c0c66f0b424eb8ab94b6cca16e00
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:55:37 -0300

    eth to puffs

--- tests/gen_btheader.go
+++ tests/gen_btheader.go
@@ -6,10 +6,10 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common/math"
+	"github.com/puffscoin/go-puffscoin/core/types"
 )
 
 var _ = (*btHeaderMarshaling)(nil)
