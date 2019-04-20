3a406320f4ac5d07558ce34e25838ccce0e4f6b1
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:25:14 -0300

    eth to puffs

--- swarm/api/encrypt.go
+++ swarm/api/encrypt.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ import (
 	"encoding/binary"
 	"errors"
 
-	"github.com/ethereum/go-ethereum/swarm/storage/encryption"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/encryption"
 	"golang.org/x/crypto/sha3"
 )
 
