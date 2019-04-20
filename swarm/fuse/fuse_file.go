48cba915645c1247a5176afb308eb9e8ffc5ffc6
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:44:57 -0300

    eth to puffs

--- swarm/fuse/fuse_file.go
+++ swarm/fuse/fuse_file.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -26,8 +26,8 @@ import (
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
-	"github.com/ethereum/go-ethereum/swarm/log"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/log"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
 	"golang.org/x/net/context"
 )
 
