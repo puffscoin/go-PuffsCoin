102a6e42ef1ffbfc175e0015dbc9f97b2974870e
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:57:40 -0300

    eth to puffs

--- tests/gen_sttransaction.go
+++ tests/gen_sttransaction.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common/math"
 )
 
 var _ = (*stTransactionMarshaling)(nil)
