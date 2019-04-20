7ae82837336024d49669b16af581bbc3673fc71c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:26:30 -0300

    eth to puffs

--- swarm/storage/mock/mem/mem_test.go
+++ swarm/storage/mock/mem/mem_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ package mem
 import (
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/storage/mock/test"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/mock/test"
 )
 
 // TestGlobalStore is running test for a GlobalStore
