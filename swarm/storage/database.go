471f814464bff7055bfc581f20608454f9ad880b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:05:16 -0300

    eth to puffs

--- swarm/storage/database.go
+++ swarm/storage/database.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ package storage
 // no need for queueing/caching
 
 import (
-	"github.com/ethereum/go-ethereum/metrics"
+	"github.com/puffscoin/go-puffscoin/metrics"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
 	"github.com/syndtr/goleveldb/leveldb/opt"
