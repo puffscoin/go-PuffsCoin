5e0ca734fbe7cb7108786f7c944bac75f599f7f9
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:15:06 -0300

    eth to puffs

--- swarm/storage/feed/update.go
+++ swarm/storage/feed/update.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/ethereum/go-ethereum/swarm/chunk"
+	"github.com/puffscoin/go-puffscoin/swarm/chunk"
 )
 
 // ProtocolVersion defines the current version of the protocol that will be included in each update message
