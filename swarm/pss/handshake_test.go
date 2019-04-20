a4998bcc8eaab179fde8112b1fa8b3695a9525a6
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 14:31:39 -0300

    eth to puffs

--- swarm/pss/handshake_test.go
+++ swarm/pss/handshake_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,7 +22,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/log"
+	"github.com/puffscoin/go-puffscoin/swarm/log"
 )
 
 // asymmetrical key exchange between two directly connected peers
