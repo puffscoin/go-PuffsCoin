a0d6162e3828bad6a83165d68cccd91ae2a69236
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:13:27 -0300

    eth to puffs

--- swarm/storage/feed/sign.go
+++ swarm/storage/feed/sign.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,8 +19,8 @@ package feed
 import (
 	"crypto/ecdsa"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/crypto"
 )
 
 const signatureLength = 65
