513b81bc8287088920e94e2425fa9b726f014245
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:28:16 -0300

    eth to puffs

--- swarm/api/manifest_test.go
+++ swarm/api/manifest_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -25,7 +25,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
 )
 
 func manifest(paths ...string) (manifestReader storage.LazySectionReader) {
