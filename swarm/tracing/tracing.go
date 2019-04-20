ebf1fbbd64bf791bc6431c0b71406d8ae97d07ba
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:42:08 -0300

    eth to puffs

--- swarm/tracing/tracing.go
+++ swarm/tracing/tracing.go
@@ -8,8 +8,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/spancontext"
+	"github.com/puffscoin/go-puffscoin/log"
+	"github.com/puffscoin/go-puffscoin/swarm/spancontext"
 
 	opentracing "github.com/opentracing/opentracing-go"
 	jaeger "github.com/uber/jaeger-client-go"
