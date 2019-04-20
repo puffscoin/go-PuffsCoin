6573d77cb5315aff3143f08653a7c675ff5f6f2a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:27:10 -0300

    eth to puffs

--- swarm/api/inspector.go
+++ swarm/api/inspector.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,8 +20,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/swarm/network"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/network"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
 )
 
 type Inspector struct {
