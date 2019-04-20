8e6e58bc5dcf5f7c73a033809a4946e5f8932def
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:21:58 -0300

    eth to puffs

--- swarm/storage/localstore/subscription_pull_test.go
+++ swarm/storage/localstore/subscription_pull_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -24,7 +24,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/chunk"
+	"github.com/puffscoin/go-puffscoin/swarm/chunk"
 )
 
 // TestDB_SubscribePull uploads some chunks before and after
