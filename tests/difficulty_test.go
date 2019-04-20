a6715911078467357f364957aacaffa9833f65d0
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:54:17 -0300

    eth to puffs

--- tests/difficulty_test.go
+++ tests/difficulty_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,8 +20,8 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/params"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/params"
 )
 
 var (
