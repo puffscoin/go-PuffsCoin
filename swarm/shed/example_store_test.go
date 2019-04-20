8f9062f1d99cc124b072ac176f4737fdab7baeef
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 17:56:28 -0300

    eth to puffs

--- swarm/shed/example_store_test.go
+++ swarm/shed/example_store_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -26,8 +26,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/shed"
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/shed"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
