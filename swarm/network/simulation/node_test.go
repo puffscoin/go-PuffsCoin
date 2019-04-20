6397363f39a58b69421a9860c21201b6a2d21ec8
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:58:23 -0300

    eth to puffs

--- swarm/network/simulation/node_test.go
+++ swarm/network/simulation/node_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,12 +23,12 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/ethereum/go-ethereum/p2p/simulations"
-	"github.com/ethereum/go-ethereum/p2p/simulations/adapters"
-	"github.com/ethereum/go-ethereum/swarm/network"
+	"github.com/puffscoin/go-puffscoin/log"
+	"github.com/puffscoin/go-puffscoin/node"
+	"github.com/puffscoin/go-puffscoin/p2p/enode"
+	"github.com/puffscoin/go-puffscoin/p2p/simulations"
+	"github.com/puffscoin/go-puffscoin/p2p/simulations/adapters"
+	"github.com/puffscoin/go-puffscoin/swarm/network"
 )
 
 func TestUpDownNodeIDs(t *testing.T) {
