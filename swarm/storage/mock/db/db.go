34967c59c6618a77984de37b4bc5ae3b3cebe06a
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:23:26 -0300

    eth to puffs

--- swarm/storage/mock/db/db.go
+++ swarm/storage/mock/db/db.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -31,8 +31,8 @@ import (
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/util"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/swarm/storage/mock"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/mock"
 )
 
 // GlobalStore contains the LevelDB database that is storing
