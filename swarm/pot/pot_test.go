9e5f4d983edec8eae3cc2de44e2b4ab8e80ca811
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 14:11:07 -0300

    eth to puffs

--- swarm/pot/pot_test.go
+++ swarm/pot/pot_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -24,7 +24,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/log"
+	"github.com/puffscoin/go-puffscoin/swarm/log"
 )
 
 const (
