87130a24168dca1b104caf3f5246b6a146c5e20c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:20:11 -0300

    eth to puffs

--- swarm/storage/localstore/mode_put_test.go
+++ swarm/storage/localstore/mode_put_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -23,7 +23,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ethereum/go-ethereum/swarm/chunk"
+	"github.com/puffscoin/go-puffscoin/swarm/chunk"
 )
 
 // TestModePutRequest validates ModePutRequest index values on the provided DB.
