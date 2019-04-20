5250fa151888c22a3af267d8693495a406315c70
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:56:02 -0300

    eth to puffs

--- tests/gen_difficultytest.go
+++ tests/gen_difficultytest.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/math"
 )
 
 var _ = (*difficultyTestMarshaling)(nil)
