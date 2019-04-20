665c9f5ecf8d64f33b301079782e0dd927250b20
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:28:32 -0300

    eth to puffs

--- swarm/storage/mock/test/test.go
+++ swarm/storage/mock/test/test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -26,9 +26,9 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/swarm/storage"
-	"github.com/ethereum/go-ethereum/swarm/storage/mock"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/mock"
 )
 
 // MockStore creates NodeStore instances from provided GlobalStorer,
