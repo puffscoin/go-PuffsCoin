b2293f1e068e862d2940b5e63587b69145f6cf77
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:17:50 -0300

    eth to puffs

--- swarm/storage/localstore/localstore.go
+++ swarm/storage/localstore/localstore.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,10 +22,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/ethereum/go-ethereum/log"
-	"github.com/ethereum/go-ethereum/swarm/chunk"
-	"github.com/ethereum/go-ethereum/swarm/shed"
-	"github.com/ethereum/go-ethereum/swarm/storage/mock"
+	"github.com/puffscoin/go-puffscoin/log"
+	"github.com/puffscoin/go-puffscoin/swarm/chunk"
+	"github.com/puffscoin/go-puffscoin/swarm/shed"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/mock"
 )
 
 var (
