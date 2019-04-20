182ef739f1f82dd2d3c7e0b70a1a1c099b7c3e5c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:30:09 -0300

    eth to puffs

--- swarm/api/uri_test.go
+++ swarm/api/uri_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -21,7 +21,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/storage"
+	"github.com/puffscoin/go-puffscoin/swarm/storage"
 )
 
 func TestParseURI(t *testing.T) {
