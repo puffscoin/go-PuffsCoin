c59c56a338a321c6097313ddc3ecc13a8c308f6b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:19:18 -0300

    eth to puffs

--- swarm/storage/localstore/mode_has.go
+++ swarm/storage/localstore/mode_has.go
@@ -1,5 +1,5 @@
 // Copyright 2019 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
 package localstore
 
 import (
-	"github.com/ethereum/go-ethereum/swarm/chunk"
+	"github.com/puffscoin/go-puffscoin/swarm/chunk"
 )
 
 // Hasser provides Has method to retrieve Chunks
