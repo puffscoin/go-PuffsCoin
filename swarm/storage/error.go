7308984eb10fcdba57fa991851f723a677e86ead
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 19:05:45 -0300

    eth to puffs

--- swarm/storage/error.go
+++ swarm/storage/error.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -16,7 +16,7 @@
 
 package storage
 
-import "github.com/ethereum/go-ethereum/swarm/chunk"
+import "github.com/puffscoin/go-puffscoin/swarm/chunk"
 
 const (
 	ErrInit = iota
