bd4a0061c3daf839f1b8436ebf0208290d8d78d9
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 12:52:30 -0300

    eth to puffs

--- swarm/network/simulation/bucket.go
+++ swarm/network/simulation/bucket.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -16,7 +16,7 @@
 
 package simulation
 
-import "github.com/ethereum/go-ethereum/p2p/enode"
+import "github.com/puffscoin/go-puffscoin/p2p/enode"
 
 // BucketKey is the type that should be used for keys in simulation buckets.
 type BucketKey string
