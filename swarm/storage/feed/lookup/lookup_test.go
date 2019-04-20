2338ab3668adac4b9615b745eed4a71c5ae1aad3
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:07:07 -0300

    eth to puffs

--- swarm/storage/feed/lookup/lookup_test.go
+++ swarm/storage/feed/lookup/lookup_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -22,8 +22,8 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/log"
-	"github.com/ethereum/go-ethereum/swarm/storage/feed/lookup"
+	"github.com/puffscoin/go-puffscoin/swarm/log"
+	"github.com/puffscoin/go-puffscoin/swarm/storage/feed/lookup"
 )
 
 type Data struct {
