4487330a8998b7c7b2f37b7a4c0f8298046fdd8b
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-20 18:21:04 -0300

    eth to puffs

--- swarm/storage/localstore/retrieval_index_test.go
+++ swarm/storage/localstore/retrieval_index_test.go
@@ -1,5 +1,5 @@
 // Copyright 2018 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/swarm/chunk"
+	"github.com/puffscoin/go-puffscoin/swarm/chunk"
 )
 
 // BenchmarkRetrievalIndexes uploads a number of chunks in order to measure
