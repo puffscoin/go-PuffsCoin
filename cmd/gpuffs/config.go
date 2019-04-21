4707e23fd33d421eacb7714d0426c561eca6f629
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-21 17:21:53 -0300

    Update config.go
    geth to gpuffs

--- cmd/gpuffs/config.go
+++ cmd/gpuffs/config.go
@@ -1,18 +1,22 @@
-// Copyright 2019 The go-puffscoin Authors
+// Copyright 2019 The go-ethereum Authors
 // This file is part of go-puffscoin.
 //
-// go-puffscoin is free software: you can redistribute it and/or modify
+// go-ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-puffscoin is distributed in the hope that it will be useful,
+// go-ethereum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
+// PUFFScoin is an independent iteration of the EVM. 
+// PUFFScoin is maintained by The Leafy Cauldron Apothecary, LLC
+// http://puffscoin.leafycauldronapothecary.com
+//
 // You should have received a copy of the GNU General Public License
-// along with go-puffscoin. If not, see <http://www.gnu.org/licenses/>.
+// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
@@ -104,7 +108,7 @@ func defaultNodeConfig() node.Config {
 	cfg.Version = params.VersionWithCommit(gitCommit)
 	cfg.HTTPModules = append(cfg.HTTPModules, "eth", "shh")
 	cfg.WSModules = append(cfg.WSModules, "eth", "shh")
-	cfg.IPCPath = "geth.ipc"
+	cfg.IPCPath = "gpuffs.ipc"
 	return cfg
 }
 
@@ -181,7 +185,7 @@ func makeFullNode(ctx *cli.Context) *node.Node {
 	// Configure GraphQL if required
 	if ctx.GlobalIsSet(utils.GraphQLEnabledFlag.Name) {
 		if err := graphql.RegisterGraphQLService(stack, cfg.Node.GraphQLEndpoint(), cfg.Node.GraphQLCors, cfg.Node.GraphQLVirtualHosts, cfg.Node.HTTPTimeouts); err != nil {
-			utils.Fatalf("Failed to register the Ethereum service: %v", err)
+			utils.Fatalf("Failed to register the PUFFScoin service: %v", err)
 		}
 	}
 
