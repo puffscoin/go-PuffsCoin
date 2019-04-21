77fee583f70d811d8478c9142068074938d0564d
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-21 20:27:19 -0300

    Update faucet.go
    changed network listening port to 31313

--- cmd/faucet/faucet.go
+++ cmd/faucet/faucet.go
@@ -64,9 +64,9 @@ import (
 var (
 	genesisFlag = flag.String("genesis", "", "Genesis json file to seed the chain with")
 	apiPortFlag = flag.Int("apiport", 8080, "Listener port for the HTTP API connection")
-	ethPortFlag = flag.Int("ethport", 30303, "Listener port for the devp2p connection")
+	ethPortFlag = flag.Int("ethport", 31313, "Listener port for the devp2p connection")
 	bootFlag    = flag.String("bootnodes", "", "Comma separated bootnode enode URLs to seed with")
-	netFlag     = flag.Uint64("network", 0, "Network ID to use for the Ethereum protocol")
+	netFlag     = flag.Uint64("network", 420, "Network ID to use for the PUFFScoin protocol")
 	statsFlag   = flag.String("ethstats", "", "Ethstats network monitoring auth string")
 
 	netnameFlag = flag.String("faucet.name", "", "Network name to assign to the faucet")
