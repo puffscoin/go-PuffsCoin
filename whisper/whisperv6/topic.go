797557041c0b80d5813c3551133778397b7cf962
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-21 12:29:27 -0300

    eth to puffs

--- whisper/whisperv6/topic.go
+++ whisper/whisperv6/topic.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,8 +19,8 @@
 package whisperv6
 
 import (
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/common/hexutil"
 )
 
 // TopicType represents a cryptographically secure, probabilistic partial
