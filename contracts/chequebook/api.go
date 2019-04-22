7e03351918214ca24a18a63bbf85f16e7df8ec03
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-22 20:44:49 -0300

    eth to puffs

--- contracts/chequebook/api.go
+++ contracts/chequebook/api.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -20,7 +20,7 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/puffscoin/go-puffscoin/common"
 )
 
 const Version = "1.0"
