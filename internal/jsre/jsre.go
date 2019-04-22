e9a72b72ab11c2b238f307e9313e6193c1fffe5e
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-22 20:11:07 -0300

    eth to puffs

--- internal/jsre/jsre.go
+++ internal/jsre/jsre.go
@@ -26,8 +26,8 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/internal/jsre/deps"
+	"github.com/puffscoin/go-puffscoin/common"
+	"github.com/puffscoin/go-puffscoin/internal/jsre/deps"
 	"github.com/robertkrimen/otto"
 )
 
