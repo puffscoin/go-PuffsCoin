bd5188460a478e1dbae46b359839e8cfec5644af
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-22 20:41:09 -0300

    eth to puffs

--- core/vm/contract.go
+++ core/vm/contract.go
@@ -1,5 +1,5 @@
 // Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
+// This file is part of the go-puffscoin library.
 //
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
@@ -19,7 +19,7 @@ package vm
 import (
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/puffscoin/go-puffscoin/common"
 )
 
 // ContractRef is a reference to the contract's backing object
