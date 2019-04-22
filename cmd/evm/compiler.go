bbffdfe3e8a6f99f1aa30d5dfff252e391626982
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-22 20:11:51 -0300

    eth to puffs

--- cmd/evm/compiler.go
+++ cmd/evm/compiler.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/ethereum/go-puffscoin/cmd/evm/internal/compiler"
+	"github.com/puffscoin/go-puffscoin/cmd/evm/internal/compiler"
 
 	cli "gopkg.in/urfave/cli.v1"
 )
