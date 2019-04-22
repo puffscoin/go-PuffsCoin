dda013a4683fe43e00f933bcdfbcb4d787485a93
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-22 14:03:11 -0300

    Update accountcmd.go

--- cmd/gpuffs/accountcmd.go
+++ cmd/gpuffs/accountcmd.go
@@ -1,18 +1,18 @@
-// Copyright 2019 The go-puffscoin Authors
+// Copyright 2019 The go-ethereum Authors
 // This file is part of go-puffscoin.
 //
-// go-puffscoin is free software: you can redistribute it and/or modify
+// go-ethereum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// go-puffscoin is distributed in the hope that it will be useful,
+// go-ethereum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with go-puffscoin. If not, see <http://www.gnu.org/licenses/>.
+// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
 
 package main
 
@@ -86,7 +86,7 @@ Note that exporting your key in unencrypted format is NOT supported.
 
 Keys are stored under <DATADIR>/keystore.
 It is safe to transfer the entire directory or the individual keys therein
-between ethereum nodes by simply copying.
+between puffscoin nodes by simply copying.
 
 Make sure you backup your keys regularly.`,
 		Subcommands: []cli.Command{
