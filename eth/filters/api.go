178f6afcbef359374cc675b21a0da09b9b292c0c
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-04-23 14:32:29 -0300

    Update api.go
    update ethereum to type FilterCriteria puffscoin.FilterQuery

--- eth/filters/api.go
+++ eth/filters/api.go
@@ -272,8 +272,8 @@ func (api *PublicFilterAPI) Logs(ctx context.Context, crit FilterCriteria) (*rpc
 }
 
 // FilterCriteria represents a request to create a new filter.
-// Same as ethereum.FilterQuery but with UnmarshalJSON() method.
-type FilterCriteria ethereum.FilterQuery
+// Same as puffsconi.FilterQuery but with UnmarshalJSON() method.
+type FilterCriteria puffscoin.FilterQuery
 
 // NewFilter creates a new filter and returns the filter id. It can be
 // used to retrieve logs when the state changes. This method cannot be
